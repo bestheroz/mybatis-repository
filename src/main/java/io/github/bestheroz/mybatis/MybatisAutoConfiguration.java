c936a610e4ec51c9b1b6020a5744553251354980
joony.kim <bestheroz@gmail.com>
2025-01-03 09:54:16 +0900

    🩹자동 컴포넌트 스캔되도록 재수정

--- src/main/java/io/github/bestheroz/mybatis/MybatisAutoConfiguration.java
+++ src/main/java/io/github/bestheroz/mybatis/MybatisAutoConfiguration.java
@@ -1,11 +1,23 @@
 package io.github.bestheroz.mybatis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
+@EnableConfigurationProperties(MybatisProperties.class)
 public class MybatisAutoConfiguration {
+  private static final Logger log = LoggerFactory.getLogger(MybatisAutoConfiguration.class);
+
+  @PostConstruct
+  public void init() {
+      log.info("Set 'exclude fields' from application.yml: {}", MybatisProperties.getExcludeFields());
+  }
 
   @Bean
   @ConditionalOnMissingBean
