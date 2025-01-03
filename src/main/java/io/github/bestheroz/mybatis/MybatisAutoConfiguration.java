a15e9d9e6e39bb2eae2b8dc447604e567eaa9e8c
joony.kim <bestheroz@gmail.com>
2025-01-03 09:05:53 +0900

    🎨format code

--- src/main/java/io/github/bestheroz/mybatis/MybatisAutoConfiguration.java
+++ src/main/java/io/github/bestheroz/mybatis/MybatisAutoConfiguration.java
@@ -1,15 +1,15 @@
 package io.github.bestheroz.mybatis;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MybatisAutoConfiguration {
 
-    @Bean
-    @ConditionalOnMissingBean
-    public MybatisProperties mybatisProperties() {
-        return new MybatisProperties();
-    }
+  @Bean
+  @ConditionalOnMissingBean
+  public MybatisProperties mybatisProperties() {
+    return new MybatisProperties();
+  }
 }
