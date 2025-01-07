3e8f62dceec336f10a8e22036ef6efc444583477
joony.kim <bestheroz@gmail.com>
2025-01-07 14:18:50 +0900

    🧪MybatisAutoConfigurationTest 추가

---
+++ src/test/java/io/github/bestheroz/mybatis/MybatisAutoConfigurationTest.java
@@ -0,0 +1,98 @@
+package io.github.bestheroz.mybatis;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ExtendWith(MockitoExtension.class)
+class MybatisAutoConfigurationTest {
+  private static Set<String> originalExcludeFields;
+  private MybatisProperties mybatisProperties;
+
+  @Mock private Logger log;
+
+  @InjectMocks private MybatisAutoConfiguration mybatisAutoConfiguration;
+
+  @BeforeEach
+  void setUp() {
+    mybatisProperties = new MybatisProperties();
+    originalExcludeFields = new HashSet<>(MybatisProperties.getExcludeFields());
+  }
+  //
+  @AfterEach
+  void tearDown() {
+    mybatisProperties.setExcludeFields(originalExcludeFields);
+    MybatisCommand.FIELD_CACHE.clear();
+  }
+
+  @Nested
+  @DisplayName("init 메서드는")
+  class InitMethod {
+
+    @Test
+    @DisplayName("PostConstruct 어노테이션이 붙은 초기화 메서드로 exclude fields를 로깅한다")
+    void shouldLogExcludeFieldsOnInitialization() {
+      // Given
+      Set<String> excludeFields = new HashSet<>();
+      excludeFields.add("testField1");
+      excludeFields.add("testField2");
+      excludeFields = Collections.unmodifiableSet(excludeFields);
+      mybatisProperties.setExcludeFields(excludeFields);
+
+      // When
+      mybatisAutoConfiguration.init();
+
+      // Then
+      assertThat(MybatisProperties.getExcludeFields())
+          .hasSize(2)
+          .contains("testField1", "testField2");
+    }
+  }
+
+  @Nested
+  @DisplayName("mybatisProperties 메서드는")
+  class MybatisPropertiesMethod {
+
+    @Test
+    @DisplayName("새로운 MybatisProperties 인스턴스를 생성하여 반환한다")
+    void shouldCreateNewMybatisPropertiesInstance() {
+      // When
+      MybatisProperties result = mybatisAutoConfiguration.mybatisProperties();
+
+      // Then
+      assertThat(result).isNotNull().isInstanceOf(MybatisProperties.class);
+    }
+  }
+
+  @Test
+  @DisplayName("Configuration 어노테이션이 적용되어 있다")
+  void shouldHaveConfigurationAnnotation() {
+    // When
+    Configuration annotation = MybatisAutoConfiguration.class.getAnnotation(Configuration.class);
+
+    // Then
+    assertThat(annotation).isNotNull();
+  }
+
+  @Test
+  @DisplayName("EnableConfigurationProperties 어노테이션이 MybatisProperties를 대상으로 적용되어 있다")
+  void shouldHaveEnableConfigurationPropertiesAnnotation() {
+    // When
+    EnableConfigurationProperties annotation =
+        MybatisAutoConfiguration.class.getAnnotation(EnableConfigurationProperties.class);
+
+    // Then
+    assertThat(annotation).isNotNull();
+    assertThat(annotation.value()).contains(MybatisProperties.class);
+  }
+}
